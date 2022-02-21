bbf9cc4aff130539520bc16d23fba64f1cc2198f
RobertaMatera <materaroberta11@gmail.com>
2022-02-21 17:03:43 +0100

    sort bar move on the left-top corner and add title

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,12 +12,51 @@ import { Router } from '@angular/router';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public sort!: string;
+  public games!: Array<Game>;
+  private routeSub!: Subscription;
+  private gameSub!: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private httpService: HttpService, //this is the service we've just created
+    private activatedRoute: ActivatedRoute, //this is a Angular service
+    private router: Router
+  ) {}
+
+   //this is the hook that runs the first when your component builds
+   ngOnInit(): void {
+    // we use the activatedRoute.params to subscribe to this event
+    // this event returns the params which is the type of params
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
   }
+
+  searchGames(sort: string, search?: string): void {
+    this.gameSub = this.httpService
+      .getGameList(sort, search)
+      .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+      });
+  }
+
+
 
   onSubmit(form: NgForm) {
     this.router.navigate(['search', form.value.search])
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 }
